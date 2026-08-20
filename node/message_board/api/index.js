1c405e91c07da7b364eee7ea9c1e625b2c2a3135
achouro <97984483+achouro@users.noreply.github.com>
2026-08-20 18:33:42 +0100

    Fix static file path for express middleware

--- node/message_board/api/index.js
+++ node/message_board/api/index.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, '../views'));
 app.set("view engine", "ejs")
 
 app.use(express.urlencoded({extended:true}))
-app.use(express.static('../public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 const messages=require('../messages.js')
 
