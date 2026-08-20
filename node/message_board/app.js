e913b6802f5d589bc840af7320fb79fd431edc7e
achouro <achouromar101@gmail.com>
2026-08-20 13:39:34 +0100

    Message Board Fixed

--- node/message_board/app.js
+++ node/message_board/app.js
@@ -1,8 +1,9 @@
-
+const path = require('path');
 const express =require("express")
 const app=express();
 const PORT=3000;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs")
 
 app.use(express.urlencoded({extended:true}))
